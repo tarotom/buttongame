1e16d4a5759cb6adcd8c6cc70ee21d998e1ffddc
Tomi Tarvainen <tomi.tarvainen@alten.fi>
2020-02-13 14:10:13 +0200

    Add default values

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [playerOnePoints, setPlayerOnePoints] = useState(20);
+  const [playerTwoPoints, setPlayerTwoPoints] = useState(20);
+  const [pointTillReward, setPointsTillReward] = useState(5);
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="Title">Button Game</div>
 
-        <div className="VerticalAlign">
+        <div className="Counter">
           <label>Counter</label>
-          <label></label>
+          <label>{counter}</label>
         </div>
 
         <div className="PlayerOne">
           <label>Player One</label>
-          <label>Points: </label>
-          <label>Clicks till next reward: </label>
+          <label>Points: {playerOnePoints}</label>
+          <label>Clicks till next reward: {pointTillReward}</label>
           <button>Click</button>
         </div>
 
         <div className="PlayerTwo">
-          <label>Player One</label>
-          <label>Points: </label>
-          <label>Clicks till next reward: </label>
+          <label>Player Two</label>
+          <label>Points: {playerTwoPoints}</label>
+          <label>Clicks till next reward: {pointTillReward}</label>
           <button>Click</button>
         </div>
 
