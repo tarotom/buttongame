ddb5e9b2490735222df1ffb71e7ca540e1cf669d
Tomi Tarvainen <tomi.tarvainen@alten.fi>
2020-02-13 11:44:25 +0200

    Add basic elements and styling to the game view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Button Game</div>
+        <div className="Title">Button Game</div>
+
+        <div className="PlayerOne">
+          <label>Player One</label>
+          <label>Points: </label>
+          <button>Click</button>
+        </div>
+
+        <div className="PlayerTwo">
+          <label>Player One</label>
+          <label>Points: </label>
+          <button>Click</button>
+        </div>
+
       </header>
     </div>
   );
